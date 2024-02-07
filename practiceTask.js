ca330ad5a1e966d7327336311d60a668da525f29
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-07 08:56:19 +0600

    Still practicing tasks of this module

--- practiceTask.js
+++ practiceTask.js
@@ -14,14 +14,62 @@ function lowestHeight(heights){
 // const height = lowestHeight(heights2);
 // console.log(height);
 
-const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed', "AR"];
 
 function smallestName(names){
-    const smallest = names[0]
+    let smallest = names[0]
     for(const name of names){
-        console.log(name);
+        // console.log(name);
+        if(name.length < smallest.length){
+            smallest = name;
+        }
     }
+    return smallest;
 }
 
 const name = smallestName(heights2);
-console.log(name);
+// console.log(name);
+
+function calculateElectronicsBudget(laptop, tablets, mobile){
+    const laptopPrice = 35000;
+    const tabletPrice = 15000;
+    const mobilePrice = 20000;
+
+    if(typeof laptop !== "number" || typeof tablets !== "number" || typeof mobile !== "number"){
+        return "Please provide a number";
+    }
+    else if(laptop < 0 || tablets < 0 || mobile < 0){
+        return "Please provide a valid number";
+    }
+
+    const laptopTotal = (laptop * laptopPrice);
+    const tabletTotal = (tablets * tabletPrice);
+    const mobileTotal = (mobile * mobilePrice);
+
+    const total = laptopTotal + tabletTotal + mobileTotal;
+    return total
+}
+
+const budget = calculateElectronicsBudget(1, 2, 4);
+// console.log(budget);
+
+const phones = [
+    { model: "PhoneA", brand: "Iphone", price: 95000 },
+    { model: "PhoneB", brand: "Samsung", price: 40000 },
+    { model: "PhoneC", brand: "Oppo", price: 26000 },
+    { model: "PhoneD", brand: "Nokia", price: 35000 },
+    { model: "PhoneE", brand: "Iphone", price: 105000 },
+    { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+function findAveragePhonePrice(mobiles){
+    // console.log(mobiles);
+    console.log(mobiles.length);
+    for(const mobile of mobiles){
+        // console.log(mobile);
+        // console.log(mobile.length);
+    }
+}
+
+const phone = findAveragePhonePrice(phones);
+console.log(phone);

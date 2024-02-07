8a5b0833e1150b59fdb8eb6e505a22540e299c3a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-07 09:55:08 +0600

    Practice tasks of this module finished

--- practiceTask.js
+++ practiceTask.js
@@ -64,12 +64,53 @@ const phones = [
 
 function findAveragePhonePrice(mobiles){
     // console.log(mobiles);
-    console.log(mobiles.length);
+    // console.log(mobiles.length);
+    if(!Array.isArray(mobiles)){
+        return "Please provide an array."
+    }
+
+    let sum = 0;
     for(const mobile of mobiles){
         // console.log(mobile);
         // console.log(mobile.length);
+        // console.log(mobile.price);
+        if(!mobile.price){
+            return "Smartphone price should be included."
+        }
+        else if(typeof mobile.price !== "number"){
+            return "Phone price should be in number."
+        }
+        else if(mobile.price <= 0){
+            return "Please provide a valid price."
+        }
+        sum = sum + mobile.price;
     }
+    // return sum;
+    let average = (sum / mobiles.length);
+    return average;
 }
 
 const phone = findAveragePhonePrice(phones);
-console.log(phone);
+// console.log(phone);
+
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+function monthlySalary(infos){
+    // console.log(infos);
+    let totalSalary = 0;
+    for(const info of infos){
+        // console.log(info);
+        const employeeSalary = (info.experience * info.increment) + info.starting;
+        // console.log(employeeSalary);
+        totalSalary = totalSalary + employeeSalary;
+    }
+    return totalSalary;
+}
+
+const salary = monthlySalary(employees);
+console.log(salary);
